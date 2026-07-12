9dcc68f8ef892bf93783957815b657bc6c857a11
Luís Henrique <luish.fa1515@gmail.com>
2026-07-12 20:59:30 -0300

    back/feat: user model

---
+++ internal/domain/user/model.go
@@ -0,0 +1,25 @@
+package user
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+type User struct {
+	ent.Schema
+}
+
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id").Unique().Immutable(),
+		field.String("name"),
+		field.String("email").Unique(),
+		field.String("password"),
+		field.String("public_id").Unique(),
+		field.Time("created_at"),
+	}
+}
+
+func (User) Edges() []ent.Edge {
+	return nil
+}
